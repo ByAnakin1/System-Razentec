1fb5bb3ba5904f28b73a41405be7473e49809331
ByAnakin1 <byhenrry1@gmail.com>
2026-03-10 13:26:15 -0500

    Forzando upload.any() para solucionar error de Multer

--- backend/src/routes/productosRoutes.js
+++ backend/src/routes/productosRoutes.js
@@ -5,17 +5,19 @@ const verifyToken = require('../middlewares/authMiddleware');
 const requireModificador = require('../middlewares/requireModificador');
 const multer = require('multer');
 
-// ✨ Usamos memoria RAM para enviar directo a Supabase
+// ✨ Usamos memoria RAM en lugar del disco duro
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 router.use(verifyToken);
 
-// Rutas sin audit automático para evitar los logs feos
 router.get('/', productosController.listar);
-router.post('/', requireModificador('Inventario'), upload.single('imagen_archivo'), productosController.crear);
+
+// ✨ LA MAGIA: upload.any() para que no bloquee ningún nombre de archivo
+router.post('/', requireModificador('Inventario'), upload.any(), productosController.crear);
 router.post('/bulk', requireModificador('Inventario'), productosController.crearGranel);
-router.put('/:id', requireModificador('Inventario'), upload.single('imagen_archivo'), productosController.actualizar);
+router.put('/:id', requireModificador('Inventario'), upload.any(), productosController.actualizar);
+
 router.delete('/:id', requireModificador('Inventario'), productosController.eliminar);
 
 module.exports = router;
